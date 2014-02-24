3f9ff698e6d9984ad70bab2b424df72c4857fac9
matthewjkelly <matthew@mjkelly.co.uk>
2014-02-24 13:15:12 +0000

    Initial Commit

---
+++ WD1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WD1
+//
+//  Created by matthew kelly on 24/02/2014.
+//  Copyright (c) 2014 matthew kelly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "mjkAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([mjkAppDelegate class]));
+    }
+}
---
+++ WD1Tests/WD1Tests.m
@@ -0,0 +1,34 @@
+//
+//  WD1Tests.m
+//  WD1Tests
+//
+//  Created by matthew kelly on 24/02/2014.
+//  Copyright (c) 2014 matthew kelly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WD1Tests : XCTestCase
+
+@end
+
+@implementation WD1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
